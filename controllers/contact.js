2884e4796777957d05ad19c832bba9a21c88204a
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-02-06 14:13:54 +0200

    migrated to hexi

--- controllers/contact.js
+++ controllers/contact.js
@@ -7,51 +7,71 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-/**
- * GET /contact
- * Contact form page.
- */
-exports.getContact = function(req, res) {
-  res.render('contact', {
-    title: 'Contact'
+module.exports = function(server, opts) {
+  /**
+   * GET /contact
+   * Contact form page.
+   */
+  server.route({
+    method: 'GET',
+    path: '/contact',
+    config: {
+      auth: false,
+    },
+    handler: function(req, res) {
+      res.render('contact', {
+        title: 'Contact'
+      });
+    },
+  });
+
+  /**
+   * POST /contact
+   * Send a contact form via Nodemailer.
+   */
+  server.route({
+    method: 'POST',
+    path: '/contact',
+    config: {
+      auth: false,
+    },
+    handler: function(req, res) {
+      req.assert('name', 'Name cannot be blank').notEmpty();
+      req.assert('email', 'Email is not valid').isEmail();
+      req.assert('message', 'Message cannot be blank').notEmpty();
+
+      var errors = req.validationErrors();
+
+      if (errors) {
+        req.flash('errors', errors);
+        return res.redirect('/contact');
+      }
+
+      var from = req.body.email;
+      var name = req.body.name;
+      var body = req.body.message;
+      var to = 'your@email.com';
+      var subject = 'Contact Form | Hackathon Starter';
+
+      var mailOptions = {
+        to: to,
+        from: from,
+        subject: subject,
+        text: body
+      };
+
+      transporter.sendMail(mailOptions, function(err) {
+        if (err) {
+          req.flash('errors', { msg: err.message });
+          return res.redirect('/contact');
+        }
+        req.flash('success', { msg: 'Email has been sent successfully!' });
+        res.redirect('/contact');
+      });
+    },
   });
 };
 
-/**
- * POST /contact
- * Send a contact form via Nodemailer.
- */
-exports.postContact = function(req, res) {
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('message', 'Message cannot be blank').notEmpty();
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/contact');
-  }
-
-  var from = req.body.email;
-  var name = req.body.name;
-  var body = req.body.message;
-  var to = 'your@email.com';
-  var subject = 'Contact Form | Hackathon Starter';
-
-  var mailOptions = {
-    to: to,
-    from: from,
-    subject: subject,
-    text: body
-  };
-
-  transporter.sendMail(mailOptions, function(err) {
-    if (err) {
-      req.flash('errors', { msg: err.message });
-      return res.redirect('/contact');
-    }
-    req.flash('success', { msg: 'Email has been sent successfully!' });
-    res.redirect('/contact');
-  });
+module.exports.attributes = {
+  name: 'web/contact',
 };
--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,18 @@
-/**
- * GET /
- * Home page.
- */
-exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
+module.exports = function(server, opts) {
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: false,
+    },
+    handler: function(req, res) {
+      res.render('home', {
+        title: 'Home',
+      });
+    },
   });
+};
+
+module.exports.attributes = {
+  name: 'web/home',
 };
